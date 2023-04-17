c224fc0a453baa268af283d66ddadc03cc63bd3c
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-04-18 00:20:15 +0530

    added check if user is registered

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,20 +4,39 @@ const UserModel = require("../models/UserModel");
 const signUpController = asyncHandler(async (req, res, next) => {
   const { firstName, email } = req.body;
 
-  console.log(req.body);
-
   if (!firstName || !email) {
     res.status(400);
     return next(new Error("All fields are required!"));
-  } else {
-    const user = await UserModel.create({ firstName, email });
-    console.log(user);
-
-    res.json({
-      success: true,
-      message: "User created in the database",
-    });
   }
+
+  const userAvailable = await UserModel.findOne({ email: email });
+
+  if (userAvailable) {
+    res.status(400);
+    return next(new Error("User already registered!"));
+  }
+
+  const user = await UserModel.create({ firstName, email });
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User created in the database",
+  });
 });
 
-module.exports = { signUpController };
+const loginController = asyncHandler(async (req, res, next) => {
+  res.json({
+    success: true,
+    message: "User logged in!",
+  });
+});
+
+const logoutController = asyncHandler(async (req, res, next) => {
+  res.json({
+    success: true,
+    message: "User logged out!",
+  });
+});
+
+module.exports = { signUpController, loginController, logoutController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUpController } = require("../controllers/authController");
+const {
+  signUpController,
+  loginController,
+  logoutController,
+} = require("../controllers/authController");
 
 router.post("/sign-up", signUpController);
+
+router.post("/login", loginController);
+
+router.post("/logout", logoutController);
 
 module.exports = router;
